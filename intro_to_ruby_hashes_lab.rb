f510d82715566300cd91503f07366b5983913041
Jaryd Hermann <hermannjaryd@gmail.com>
2021-02-16 12:43:46 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,14 +22,14 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
-      :rent_in_dollars=>
+      :rent_in_dollars =>
         {
-          :one_piece_owned=>25,
-          :two_pieces_owned=>50,
-          :three_pieces_owned=>100,
-          :four_pieces_owned=>200
+          :one_piece_owned => 25,
+          :two_pieces_owned => 50,
+          :three_pieces_owned => 100,
+          :four_pieces_owned => 200
         },
-      :names=>
+      :names =>
         {
           :reading_railroad => {},
           :pennsylvania_railroad => {},
